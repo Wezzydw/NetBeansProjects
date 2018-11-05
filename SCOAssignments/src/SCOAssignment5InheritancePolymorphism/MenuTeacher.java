3b993c444f8365b5eb197f2d53a2e804dbbdeac8
Wezzydw <Criller2@hotmail.com>
2018-11-05 12:31:34 +0100

    Added MenuTeacher

---
+++ SCOAssignments/src/SCOAssignment5InheritancePolymorphism/MenuTeacher.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SCOAssignment5InheritancePolymorphism;
+
+/**
+ *
+ * @author WezzyLaptop
+ */
+public class MenuTeacher extends Menu
+{
+
+    public MenuTeacher(String header, String[] menuItems)
+    {
+        super(header, menuItems);
+    }
+
+    @Override
+    protected void doAction(int option)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
